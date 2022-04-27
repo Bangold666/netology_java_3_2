620d7035f5220bcf2ba5515b88588773fa0e418c
Shabalin Sergey <sergeyshm38@gmail.com>
2022-04-27 20:22:11 +0300

    initial commmit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public float calculate (int weight){
+    public float calculate(int weight) {
         float height = 1.71f;
         float index = weight / (height * height);
         return index;
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int bodyMass= 70;
+        int bodyMass = 70;
         float bodyIndex = service.calculate(bodyMass);
         System.out.println(bodyIndex);
     }
