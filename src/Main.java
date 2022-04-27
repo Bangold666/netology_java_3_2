38acc6ab06aac876c6a557fe048b6c50348ad90c
Shabalin Sergey <sergeyshm38@gmail.com>
2022-04-27 20:20:35 +0300

    initial commmit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int bodyMass= 70;
+        float bodyIndex = service.calculate(bodyMass);
+        System.out.println(bodyIndex);
+    }
+}
